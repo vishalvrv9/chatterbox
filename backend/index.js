90d558bff463a426d2ac271c78d44ba726b2af53
Vishal Raj <vishalraj1802@gmail.com>
2017-11-04 01:45:32 +0530

    frontend functionality+backend init

---
+++ backend/index.js
@@ -0,0 +1,7 @@
+var express = require('express');
+
+var app = express();
+
+server = app.listen(8080, function(){
+  console.log('server is up and running on 8080')
+});
--- src/Chat.js
+++ src/Chat.js
@@ -1,6 +1,18 @@
 import React from 'react'
 
 class Chat extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      username: '',
+      message: '',
+      messageList: []
+    }
+  }
+
+
+
+
   render(){
     return(
       <div className='container'>
@@ -11,13 +23,18 @@ class Chat extends React.Component{
                 <div className="card-title"><u><b>Chat Messages</b></u></div>
                 <hr/>
                 <div className='messages'>
+                {this.state.messageList.map((message) =>
+                  {return(
+                    <div> {message.author} : {message.message} </div>
+                  )})
+                }
 
 
                 </div>
               </div>
               <div className="card-footer">
-                <input type="text" placeholder="Username" className="form-control" /> <br/>
-                <input type="text" placeholder="Your Message" className="form-control" /> <br/>
+                <input type="text" placeholder="Username" className="form-control" value={this.state.username} onChange={e => this.setState({username: e.target.value})}/> <br/>
+                <input type="text" placeholder="Your Message" value={this.state.message} className="form-control" onChange={e => this.setState({message: e.target.value})}/> <br/>
                 <button className="btn btn-primary form-control">Send Message </button>
               </div>
             </div>
